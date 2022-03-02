fb66819d3482d497ff9eef0952918c5ede61a4c5
Alexandre.oliveira <alexandre.oliveira@celeritech.com.br>
2022-03-02 13:39:32 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AA Digital Design</title>
+    <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;1,400&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <section class="start">
+        <div class="mask">
+            <header>
+                <div class="container-default">
+                    <div class="row pt-4">
+                        <div class="col">
+                            <img src="./assets/img/Logo AA Digital Design.png" alt="Logo_AA_Digital_Design">
+                        </div>
+                        <div class="col">
+                            <nav class="nav float-end">
+                                <a class="nav-link text-white" aria-current="page" href="#">Home</a>
+                                <a class="nav-link text-white" href="#">Serviços</a>
+                                <a class="nav-link text-white" href="#">Portifólio</a>
+                                <a class="nav-link text-white" href="#">Contato</a>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <div class="content">
+                <div class="container-default">
+                    <div class="content-text">
+                        <h1>O Lugar certo para desenvolver a sua identidade no mundo digital.</h1>
+                        <button class="btn btn-custom btn-lg">Fale Agora Conosco</button>
+                        <img src="./assets/img/Icone Whatsapp AA digital.png" alt="AA_digital_design_whatsapp" class="bt-whatsapp">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>

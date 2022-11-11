eb123a5328d540faf5c7d9544ff3e80e878a879f
Alexandre.oliveira <alexandre.oliveira@celeritech.com.br>
2022-11-11 17:16:54 -0300

    feat: architecture

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const port = 3000
 
 const app = express()
 
+//Função de invocação dos partials
+const hbs = exphbs.create({
+    partialsDir: ['views/partials']
+})
+
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
+//Definindo raiz padrão para importação de arquivos estáticos 
+app.use(express.static('./'))
+
+//Rotas
 app.get('/', function(req, res) {
     res.render('home')
 })

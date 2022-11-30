f2d0262f8b4ba39eda24082d01de345be72a4431
Alexandre.oliveira <alexandre.oliveira@celeritech.com.br>
2022-11-29 21:52:13 -0300

    feat: submit form route and notification response

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express from "express"
 import exphbs from "express-handlebars"
 const port = 3000
 
@@ -12,26 +12,50 @@ const hbs = exphbs.create({
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
+//Enable Json
+/* app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json()) */
+
 //Setting default root for importing static files
 app.use(express.static('./'))
 
 //Routes
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     const portfolio = ['Site Aliança Portuguesa', 'Edifica Representações', 'E-commerce Fábrica de Sintéticos', 'Social Mídia Royal Kllumm', 'Social Mídia Space lanches', 'Design de Logos']
     const titlePage = 'Home'
-    res.render('home',{portfolio: portfolio , titlePage: titlePage})
+    res.render('home', { portfolio: portfolio, titlePage: titlePage })
 })
 
-app.get('/portifolio', function(req, res) {
+app.get('/portifolio', function (req, res) {
     const titlePage = 'Portifólio'
-    res.render('portfolio',{titlePage: titlePage})
+    res.render('portfolio', { titlePage: titlePage })
 })
 
-app.get('/contato', function(req, res) {
+app.get('/contato', function (req, res) {
+    const notification = req.params.notif
+    console.log(notification)
     const titlePage = 'Contato'
-    res.render('contact', {titlePage: titlePage})
+    res.render('contact', { titlePage: titlePage })
 })
 
-app.listen(port, function() {
+app.post('/submit', function (req, res) {
+    try {
+        const name = req.body.name
+        const email = req.body.email
+        const subject = req.body.subject
+        const massage = req.body.massage
+        console.log(name, email, subject, massage)
+        let notificationTrue = true
+        res.render('contact', {notificationTrue: notificationTrue})
+    } catch (e) {
+        let notificationFalse = true
+        res.render('contact', {notificationFalse: notificationFalse})
+        console.log("ERROR")
+    }
+})
+
+app.listen(port, function () {
     console.log(`Server running on port ${port}`)
 })
